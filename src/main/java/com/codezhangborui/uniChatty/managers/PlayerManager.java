b1759926a98b1633cef50fd29070508e1f922f89
CodeZhangBorui <zhangborui.2008@foxmail.com>
2026-07-16 08:07:25 +0800

    fix: Convert LegacyMessage to ModernComponent

--- src/main/java/com/codezhangborui/uniChatty/managers/PlayerManager.java
+++ src/main/java/com/codezhangborui/uniChatty/managers/PlayerManager.java
@@ -1,13 +1,18 @@
 package com.codezhangborui.uniChatty.managers;
 
 import com.velocitypowered.api.proxy.Player;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 
 public class PlayerManager {
     public static String getPlayerPrefix(Player player) {
+        String playerPrefix = null;
         if(ConfigManager.getBoolean("connectors.luckperms")) {
-            return com.codezhangborui.uniChatty.connectors.LuckPermsConnector.getPlayerPrefix(player);
-        } else {
-            return null;
+            playerPrefix = com.codezhangborui.uniChatty.connectors.LuckPermsConnector.getPlayerPrefix(player);
         }
+        if(playerPrefix == null) return null;
+        else return MiniMessage.miniMessage().serialize(
+                LegacyComponentSerializer.legacySection().deserialize(playerPrefix)
+        ) + "<reset>";
     }
 }
